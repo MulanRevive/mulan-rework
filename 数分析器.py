4d799f1df0ee5bc14f68c79f80b5ccfcd04bfe5f
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-04-12 20:21:19 -0700

    中文化

--- 数分析器.py
+++ 数分析器.py
@@ -26,15 +26,24 @@ from rply.token import BaseBox
 @分析器母机.production('表达式 : 数')
 def 数表达式(片段):
     数 = int(片段[0].getstr(), 0)
-    return ast.Module(
-                body=[
-                    ast.Expr(
-                        value=ast.Num(数,
-                            lineno=0,
-                            col_offset=0),
-                        lineno=0,
-                        col_offset=0)
+    return 语法树_模块(
+                主体=[
+                    语法树_表达式(
+                        值 = 语法树_数(数,
+                            行号=0,
+                            列号=0),
+                        行号=0,
+                        列号=0)
                     ],
-                type_ignores=[])
+                忽略类型=[])
 
 分析器 = 分析器母机.build()
+
+def 语法树_模块(主体, 忽略类型):
+    return ast.Module(body = 主体, type_ignores = 忽略类型)
+
+def 语法树_表达式(值, 行号, 列号):
+    return ast.Expr(value = 值, lineno = 行号, col_offset = 列号)
+
+def 语法树_数(值, 行号, 列号):
+    return ast.Num(value = 值, lineno = 行号, col_offset = 列号)
