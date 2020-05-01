1015bbfd816855c50df9842fbb4fda76d3f3d142
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-04-30 17:59:09 -0700

    余下重构

--- test语法树.py
+++ test语法树.py
@@ -13,29 +13,19 @@ class test语法树(unittest.TestCase):
 
         节点 = self.生成语法树("1/0")
         #print(ast.dump(节点, True, True))
-        for module子节点 in ast.iter_fields(节点):
-            if module子节点[0] == "body":
-                expr节点 = module子节点[1][0]  # module 节点下可有多个节点
-                for expr子节点 in ast.iter_fields(expr节点):
-                    if expr子节点[0] == "value":
-                        call节点 = expr子节点[1]
-                        for call子节点 in ast.iter_fields(call节点):
-                            if call子节点[0] == "func":
-                                name节点 = call子节点[1]
-                                self.assertEqual(name节点.lineno, 1)
-                                self.assertEqual(name节点.col_offset, 2)
+        expr节点 = self.取子节点(节点, "body", 0)
+        call节点 = self.取子节点(expr节点, "value")
+        name节点 = self.取子节点(call节点, "func")
+        self.assertEqual(name节点.lineno, 1)
+        self.assertEqual(name节点.col_offset, 2)
 
         # 运算位置
         节点 = self.生成语法树("1+0")
         #print(ast.dump(节点, True, True))
-        for module子节点 in ast.iter_fields(节点):
-            if module子节点[0] == "body":
-                expr节点 = module子节点[1][0]  # module 节点下可有多个节点
-                for expr子节点 in ast.iter_fields(expr节点):
-                    if expr子节点[0] == "value":
-                        binop节点 = expr子节点[1]
-                        self.assertEqual(binop节点.lineno, 1)
-                        self.assertEqual(binop节点.col_offset, 1)
+        expr节点 = self.取子节点(节点, "body", 0)
+        binop节点 = self.取子节点(expr节点, "value")
+        self.assertEqual(binop节点.lineno, 1)
+        self.assertEqual(binop节点.col_offset, 1)
 
         # TODO: 第二行
 
