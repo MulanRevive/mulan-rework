c82fd722efe40683aaa37054ba75cb442d917f2c
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-05-22 20:23:48 -0700

    为高亮插件测试提供所有元素

--- 测试.py
+++ 测试.py
@@ -38,6 +38,7 @@ import subprocess
     "返回单值.ul": b"2",
     "二阶函数.ul": b"11",
     "缩进.ul": b"2",
+    "复杂.ul": b"10",
 }
 
 # 多进程参考：https://shuzhanfan.github.io/2017/12/parallel-processing-python-subprocess/
