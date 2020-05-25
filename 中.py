fca22ba8f607b11936d554ae8dc8d80e20830aa4
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-05-25 00:10:23 -0700

    错误信息初步中文化

--- 中.py
+++ 中.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import re, sys
 import ast, python
 from 词法分析器 import 分词器
 from 分析器 import 语法分析器
@@ -26,8 +26,20 @@ with open(源码文件, 'r') as f:
 #print(语法树相关.格式化节点(节点, 1))
 
 # 参考：https://docs.python.org/3.7/library/functions.html?highlight=compile#compile
-可执行码 = compile(节点, 源码文件, 'exec')
+try:
+    可执行码 = compile(节点, 源码文件, 'exec')
 
-环境变量 = 创建全局变量()
+    环境变量 = 创建全局变量()
 
-exec(可执行码, 环境变量)
+    try:
+        exec(可执行码, 环境变量)
+    except RecursionError as 递归报错:
+        sys.stderr.write("递归过深。请确认: 1、的确需要递归 2、递归的收敛正确" + '\n')
+    except ZeroDivisionError as 除零报错:
+        sys.stderr.write("请勿除零" + '\n')
+    except NameError as 未定义:
+        # 参考: https://github.com/program-in-chinese/study/blob/6084cbfc39166842b95d25d5c2fe419f1c604475/1-%E5%9F%BA%E7%A1%80/%E6%8E%A7%E5%88%B6%E5%8F%B0/%E8%A7%A3%E9%87%8A%E5%99%A8.py
+        sys.stderr.write(re.sub(r"name '(.*)' is not defined", r"'\1'没定义就使用了", str(未定义)) + '\n')
+except SyntaxError as 语法错误:
+    sys.stderr.write("语法错误: " + str(语法错误) + '\n')
+
