434e1909d32bedb3d354dfd2cee161c13fa250fe
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-06-13 01:02:38 -0700

    微重整 2

--- 中.py
+++ 中.py
@@ -5,7 +5,7 @@ import ast
 from 词法分析器 import 分词器
 from 语法分析器 import 语法分析器
 from 环境 import 创建全局变量
-from 功用.语法树相关 import 语法树相关
+from 功用.调试辅助 import 语法树相关
 from 功用.反馈信息 import 反馈信息
 from rply.errors import LexingError
 from 语法树处理 import NameFixPass
