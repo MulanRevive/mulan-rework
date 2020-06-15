df5588be7faa2aeb8aaed16d22665349a4209900
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-06-15 01:32:51 -0700

    支持列表:  一元素

--- 测试.py
+++ 测试.py
@@ -89,7 +89,7 @@ from sys import platform
     "字符串/双引号.ul": b"oka",
 
     "数据结构/范围.ul": b"range(0, 3)range(0, 3)range(0, 2)",
-    "数据结构/列表.ul": b"[]",
+    "数据结构/列表.ul": b"[][2]",
 
     "综合.ul": b"10",
     #"错误处理/死递归.ul": b"test",
