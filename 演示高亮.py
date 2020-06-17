0cf630c647f32a63f453f8bf84df4700053090b1
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-06-17 01:19:13 -0700

    编辑器可打开源码文件

--- 演示高亮.py
+++ 演示高亮.py
@@ -6,18 +6,9 @@ from 分析器 import 词法分析器
 root = Tk()
 
 # TODO: 已知问题: 输入中文符号"（"时，实际为退格，"）"时为空格。thonny 也有同样问题。
-
-源码 = """using * in 海龟
-
-颜色("黄色", "红色")
-开始填充()
-for 拐数 in 0..4 {
-  前进(200)
-  右转(144)
-}
-结束填充()
-主循环()
-"""
+源码文件 = sys.argv[1]
+with open(源码文件, 'r', encoding='utf-8') as f:
+    源码 = f.read()
 
 # 行间距设置, 参考: https://www.javatpoint.com/python-tkinter-text
 文本 = Text(root, spacing1=3, spacing3=3)
@@ -32,25 +23,26 @@ for 拐数 in 0..4 {
     "整数": "黄绿",
     "字符串字面量": "橙",
 }
+
+# 颜色表: https://www.w3schools.com/colors/colors_names.asp
+颜色表 = {
+    "紫红": "VioletRed",
+    "黄绿": "YellowGreen",
+    "橙": "orange",
+}
 各词 = 词法分析器.分词器.lex(源码)
 for 词 in 各词:
     行号 = 词.getsourcepos().lineno
-    列号 = 词.getsourcepos().colno
+    列号 = 词.getsourcepos().colno - 1
     词长 = len(词.getstr())
-    #print(词.name + " " + 词.getstr() + " " + str(行号) + "." + str(列号))
+    # print(词.name + " " + 词.getstr() + " " + str(行号) + "." + str(列号))
     if 词.name in 高亮风格:
-        文本.tag_add(高亮风格[词.name], str(行号) + "." + str(列号 - 1), str(行号) + "." + str(列号 - 1 + 词长))
+        文本.tag_add(高亮风格[词.name], f'{行号}.{列号}', f'{行号}.{列号 + 词长}')
 
-# 颜色表: https://www.w3schools.com/colors/colors_names.asp
-文本.tag_config("蓝", foreground="blue")
-文本.tag_config("红", foreground="red")
-文本.tag_config("橙", foreground="orange")
-文本.tag_config("绿黄", foreground="GreenYellow")
-文本.tag_config("黄绿", foreground="YellowGreen")
-文本.tag_config("紫红", foreground="VioletRed")
+for 颜色 in 颜色表:
+    文本.tag_config(颜色, foreground=颜色表[颜色])
 
-myFont = Font(family="Courier", size=16)
-文本.configure(font=myFont)
+文本.configure(font=Font(family="Courier", size=16))
 
 # 参考: https://www.delftstack.com/howto/python-tkinter/how-to-set-tkinter-backgroud-color/
 #root.configure(background='black')
