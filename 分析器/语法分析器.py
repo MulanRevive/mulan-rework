a6fbe4afa293fac4892d78c33872c9f5408a641d
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-06-22 19:20:59 -0700

    续前, 通过所有测试

--- 分析器/语法分析器.py
+++ 分析器/语法分析器.py
@@ -33,12 +33,12 @@ class 语法分析器:
     # ast 参考: https://docs.python.org/3.7/library/ast.html#abstract-grammar
 
     @分析器母机.production('模块 : 注水声明列表')
-    def 模块(片段):
+    def 模块(self, 片段):
         return 语法树.模块(主体=片段[0], 忽略类型=[])
 
     @分析器母机.production('块 : 分号')
     @分析器母机.production('块 : 前括号 注水声明列表 后括号')
-    def 块(片段):
+    def 块(self, 片段):
         if 语法分析器.调试:
             print('块')
         if len(片段) == 3:
@@ -50,7 +50,7 @@ class 语法分析器:
     @分析器母机.production('注水声明列表 : ')
     @分析器母机.production('注水声明列表 : 声明列表')
     @分析器母机.production('注水声明列表 : 声明列表 换行')
-    def 注水声明列表(片段):
+    def 注水声明列表(self, 片段):
         if 语法分析器.调试:
             print('注水声明列表')
         if len(片段) > 0:
@@ -74,13 +74,13 @@ class 语法分析器:
     @分析器母机.production('声明 : 每当声明')
     @分析器母机.production('声明 : 对于声明')
     @分析器母机.production('声明 : 外部声明')
-    def 混合声明(片段):
+    def 混合声明(self, 片段):
         if 语法分析器.调试:
             print('混合声明')
         return 片段[0]
 
     @分析器母机.production('类型定义 : 名词_类型 名称 各基准类 类型主体')
-    def 类型定义(片段):
+    def 类型定义(self, 片段):
         return 语法树.类定义(
             名称=片段[1].id,
             各基准类=片段[2],
@@ -89,7 +89,7 @@ class 语法分析器:
 
     @分析器母机.production('各基准类 :')
     @分析器母机.production('各基准类 : : 表达式前缀')
-    def 各基准类(片段):
+    def 各基准类(self, 片段):
         if 语法分析器.调试:
             print('各基准类')
         if len(片段) == 0:
@@ -97,14 +97,14 @@ class 语法分析器:
         return [片段[1]]
 
     @分析器母机.production('类型主体 : 前括号 各类型内声明 后括号')
-    def 类型主体(片段):
+    def 类型主体(self, 片段):
         if 语法分析器.调试:
             print('类型主体')
         return 片段[1]
 
     @分析器母机.production('各类型内声明 : ')
     @分析器母机.production('各类型内声明 : 各类型内声明 类型内声明')
-    def 各类型内声明(片段):
+    def 各类型内声明(self, 片段):
         if 语法分析器.调试:
             print('各类型内声明')
         if len(片段) == 0:
@@ -119,21 +119,21 @@ class 语法分析器:
     @分析器母机.production('类型内声明 : 块')
     @分析器母机.production('类型内声明 : 操作符')
     @分析器母机.production('类型内声明 : 函数')
-    def 类型内声明(片段):
+    def 类型内声明(self, 片段):
         if 语法分析器.调试:
             print('类型内声明')
         return 片段[0]
 
     @分析器母机.production('操作符 : 名词_操作符 二元操作符 操作数 块')
-    def 操作符(片段):
+    def 操作符(self, 片段):
         return 语法树.函数定义(名称=片段[1],
                         形参列表=片段[2],
                         主体=片段[-1],
                         片段=片段)
 
     @分析器母机.production('操作数 : ( 形参 )')
-    def 操作数(片段):
-        各形参 = 语法分析器.形参列表()
+    def 操作数(self, 片段):
+        各形参 = self.形参列表()
         各形参.args.append(语法树.形参(名称='self',
                                标注=None,
                                片段=片段))
@@ -142,7 +142,7 @@ class 语法分析器:
 
     @分析器母机.production('二元操作符 : 加')
     @分析器母机.production('二元操作符 : 減')
-    def 二元操作符(片段):
+    def 二元操作符(self, 片段):
         对照表 = {
             '+': '__add__',
             '-': '__sub__',
@@ -165,12 +165,12 @@ class 语法分析器:
 
     @分析器母机.production('模块位置 : 模块名')
     @分析器母机.production('模块位置 : 点')
-    def 模块位置(片段):
+    def 模块位置(self, 片段):
         return 片段[0]
 
     @分析器母机.production('引用声明 : 动词_引用 各模块名 连词_于 模块位置')
     @分析器母机.production('引用声明 : 动词_引用 星号 连词_于 模块位置')
-    def 引用于(片段):
+    def 引用于(self, 片段):
         模块, 层 = 片段[-1], 0
         if isinstance(片段[-1], Token):
             模块 = None
@@ -190,14 +190,14 @@ class 语法分析器:
         return 节点
 
     @分析器母机.production('引用声明 : 动词_引用 各模块名')
-    def 引用声明(片段):
+    def 引用声明(self, 片段):
         return 语法树.导入(
             名称=片段[1],
             片段=片段)
 
     @分析器母机.production('各模块名 : 模块名')
     @分析器母机.production('各模块名 : 各模块名 , 模块名')
-    def 各模块名(片段):
+    def 各模块名(self, 片段):
         别名 = 语法树.别名(
             名称=片段[-1],
             别名=None,
@@ -210,13 +210,13 @@ class 语法分析器:
 
     @分析器母机.production('模块名 : 模块名 点 名称')
     @分析器母机.production('模块名 : 名称')
-    def 模块名(片段):
+    def 模块名(self, 片段):
         if len(片段) == 1:
             return 片段[0].id
         return '%s.%s' % (片段[0], 片段[2].id)
 
     @分析器母机.production('表达式声明 : 表达式前缀')
-    def 表达式声明(片段):
+    def 表达式声明(self, 片段):
         if 语法分析器.调试:
             print("表达式声明")
         return 语法树.表达式(值 = 片段[0], 片段 = 片段)
@@ -224,7 +224,7 @@ class 语法分析器:
     # TODO: 返回(多个)值
     @分析器母机.production('返回声明 : 动词_返回')
     @分析器母机.production('返回声明 : 动词_返回 表达式')
-    def 返回(片段):
+    def 返回(self, 片段):
         值 = None
         if len(片段) == 2:
             值 = 片段[1]
@@ -234,7 +234,7 @@ class 语法分析器:
 
     @分析器母机.production('增量赋值 : 表达式前缀 += 表达式')
     @分析器母机.production('增量赋值 : 表达式前缀 -= 表达式')
-    def 增量赋值(片段):
+    def 增量赋值(self, 片段):
         运算符 = 片段[1].getstr()
         对照表 = {
             '+=': ast.Add(),
@@ -260,7 +260,7 @@ class 语法分析器:
             片段 = 片段)
 
     @分析器母机.production('外部声明 : 形容词_外部 各名称')
-    def 外部声明(片段):
+    def 外部声明(self, 片段):
         return 语法树.全局([名称.id for 名称 in 片段[1]], 片段=片段)
 
     @分析器母机.production('各表达式前缀 : 表达式前缀 , 表达式前缀')
@@ -268,7 +268,7 @@ class 语法分析器:
         return [片段[0], 片段[2]]
 
     @分析器母机.production('赋值 : 各表达式前缀 = 各表达式')
-    def 多项赋值(片段):
+    def 多项赋值(self, 片段):
         for 表达式前缀 in 片段[0]:
             表达式前缀.ctx = ast.Store()
 
@@ -290,17 +290,17 @@ class 语法分析器:
             片段=片段)
 
     @分析器母机.production('终止声明 : 动词_终止')
-    def 终止声明(片段):
+    def 终止声明(self, 片段):
         return 语法树.终止(片段 = 片段)
 
     @分析器母机.production('跳过声明 : 动词_跳过')
-    def 跳过声明(片段):
+    def 跳过声明(self, 片段):
         return 语法树.跳过(片段 = 片段)
 
     @分析器母机.production('二元表达式 : 表达式 加 表达式')
     @分析器母机.production('二元表达式 : 表达式 減 表达式')
     @分析器母机.production('二元表达式 : 表达式 星号 表达式')
-    def 二元表达式(片段):
+    def 二元表达式(self, 片段):
         左 = 片段[0]
         右 = 片段[2]
         运算符 = 片段[1].getstr()
@@ -316,7 +316,7 @@ class 语法分析器:
         return 语法树.二元运算(左, python运算, 右, 片段)
 
     @分析器母机.production('二元表达式 : 表达式 除 表达式')
-    def 除法(片段):
+    def 除法(self, 片段):
         return 语法树.调用(
             函数=语法树.名称(
                 标识='__div__',
@@ -333,7 +333,7 @@ class 语法分析器:
     @分析器母机.production('二元表达式 : 表达式 != 表达式')
     @分析器母机.production('二元表达式 : 表达式 === 表达式')
     @分析器母机.production('二元表达式 : 表达式 !== 表达式')
-    def 比较(片段):
+    def 比较(self, 片段):
         对照表 = {
             '>': ast.Gt(),
             '>=': ast.GtE(),
@@ -352,7 +352,7 @@ class 语法分析器:
 
     @分析器母机.production('二元表达式 : 表达式 连词_且 表达式')
     @分析器母机.production('二元表达式 : 表达式 连词_或 表达式')
-    def 布尔表达式(片段):
+    def 布尔表达式(self, 片段):
         return 语法树.布尔操作(
             操作符=(ast.And() if 片段[1].getstr() == 'and' else ast.Or()),
             前项 = 片段[0],
@@ -361,7 +361,7 @@ class 语法分析器:
 
     @分析器母机.production('范围表达式 : 表达式 点点 表达式')
     @分析器母机.production('范围表达式 : 表达式 点点小于 表达式')
-    def 范围表达式(片段):
+    def 范围表达式(self, 片段):
         连词 = 片段[1].getstr()
         if 连词 != 'by':
             起 = 片段[0]
@@ -381,14 +381,14 @@ class 语法分析器:
 
     # TODO: ~ -
     @分析器母机.production('一元表达式 : 非 表达式')
-    def 一元表达式(片段):
+    def 一元表达式(self, 片段):
         操作符 = 片段[0].getstr()
         if 操作符 == '!':
             操作符 = ast.Not()
         return 语法树.一元操作(操作符, 片段[1], 片段=片段)
 
     @分析器母机.production('三元表达式 : 表达式 ? 表达式 : 表达式')
-    def 三元表达式(片段):
+    def 三元表达式(self, 片段):
         return 语法树.如果表达式(
             条件=片段[0],
             主体=片段[2],
@@ -396,7 +396,7 @@ class 语法分析器:
             片段=片段)
 
     @分析器母机.production('首要表达式 : ( 表达式 )')
-    def 首要表达式(片段):
+    def 首要表达式(self, 片段):
         return 片段[1]
 
     @分析器母机.production('表达式前缀 : 变量')
@@ -409,16 +409,16 @@ class 语法分析器:
         return 片段[0]
 
     @分析器母机.production('片 : 表达式')
-    def 索引(片段):
+    def 索引(self, 片段):
         return 语法树.索引(值=片段[0], 片段=片段)
 
     @分析器母机.production('变量 : 表达式前缀 [ 片 ]')
-    def 片表达式(片段):
+    def 片表达式(self, 片段):
         return 语法树.下标(片段[0], 片段[2], 片段)
 
     # TODO: 添加测试: 调用().名称
     @分析器母机.production('变量 : 表达式前缀 点 名称')
-    def 属性表达式(片段):
+    def 属性表达式(self, 片段):
         return 语法树.属性(
             值=片段[0],
             属性=片段[2].id,
@@ -430,7 +430,7 @@ class 语法分析器:
 
     @分析器母机.production('实参部分 : ( 各实参 )')
     @分析器母机.production('实参部分 : ( )')
-    def 实参部分(片段):
+    def 实参部分(self, 片段):
         if 语法分析器.调试:
             print("实参部分")
         if len(片段) != 3:
@@ -439,7 +439,7 @@ class 语法分析器:
 
     # 变量->表达式前缀, 以支持高阶函数调用, 如 f(1)(2)
     @分析器母机.production('调用 : 表达式前缀 实参部分')
-    def 调用(片段):
+    def 调用(self, 片段):
         if 语法分析器.调试:
             print("调用")
         各参数 = []
@@ -457,7 +457,7 @@ class 语法分析器:
 
     @分析器母机.production('数 : 整数')
     @分析器母机.production('数 : 小数')
-    def 数(片段):
+    def 数(self, 片段):
         try:
             return 语法树.数(int(片段[0].getstr(), 0), 片段)
         except ValueError:
@@ -471,15 +471,15 @@ class 语法分析器:
         return 语法树.字符串(值, 片段)
 
     @分析器母机.production('常量 : 名词_真')
-    def 常量_真(片段):
+    def 常量_真(self, 片段):
         return 语法树.常量(True, 片段=片段)
 
     @分析器母机.production('常量 : 名词_假')
-    def 常量_假(片段):
+    def 常量_假(self, 片段):
         return 语法树.常量(False, 片段=片段)
 
     @分析器母机.production('常量 : 名词_空')
-    def 常量_空(片段):
+    def 常量_空(self, 片段):
         return 语法树.常量(None, 片段=片段)
 
     @分析器母机.production('表达式 : 二元表达式')
@@ -516,14 +516,14 @@ class 语法分析器:
         return [片段[0]]
 
     @分析器母机.production('实参 : 表达式')
-    def 实参(片段):
+    def 实参(self, 片段):
         if 语法分析器.调试:
             print("实参")
         return (片段[0], None)
 
     @分析器母机.production('形参列表 : ')
     @分析器母机.production('形参列表 : 非空形参列表')
-    def 形参列表(片段=[]):
+    def 形参列表(self, 片段=[]):
         if not 片段:
             return 语法树.各形参([])
         # TODO: 如支持形参默认值, 需要 legalize_arguments
@@ -531,16 +531,16 @@ class 语法分析器:
 
     @分析器母机.production('非空形参列表 : 形参')
     @分析器母机.production('非空形参列表 : 非空形参列表 , 形参')
-    def 非空形参列表(片段):
+    def 非空形参列表(self, 片段):
         if len(片段) == 1:
-            各形参 = 语法分析器.形参列表()
+            各形参 = self.形参列表()
         else:
             各形参 = 片段[0]
         各形参.args.append(片段[-1])
         return 各形参
 
     @分析器母机.production('形参 : 名称')
-    def 形参(片段):
+    def 形参(self, 片段):
         return 语法树.形参(
             名称=片段[0].id,
             标注=(None if len(片段) == 1 else 片段[-1]),
@@ -548,10 +548,10 @@ class 语法分析器:
 
     @分析器母机.production('函数 : 名词_函数 标识符 ( 形参列表 ) 块')
     @分析器母机.production('函数 : 名词_函数 标识符 块')
-    def 函数(片段):
+    def 函数(self, 片段):
         return 语法树.函数定义(
             名称=(片段[1].getstr()),
-            形参列表=(片段[3] if len(片段) == 6 else 语法分析器.形参列表()),
+            形参列表=(片段[3] if len(片段) == 6 else self.形参列表()),
             主体=片段[-1],
             片段=片段)
 
@@ -560,7 +560,7 @@ class 语法分析器:
     @分析器母机.production('否则如果声明 : ')
     @分析器母机.production('否则如果声明 : 连词_否则如果 表达式 块 否则如果声明')
     @分析器母机.production('否则如果声明 : 连词_否则如果 表达式 块 连词_否则 块')
-    def 条件声明(片段):
+    def 条件声明(self, 片段):
         if len(片段) == 0:
             return []
         否则部分 = 片段[-1]
@@ -571,7 +571,7 @@ class 语法分析器:
             片段=片段)
 
     @分析器母机.production('条件声明 : 声明 连词_如果 表达式')
-    def 条件倒置声明(片段):
+    def 条件倒置声明(self, 片段):
         return 语法树.如果(
             条件=片段[-1],
             主体=[片段[0]],
@@ -579,7 +579,7 @@ class 语法分析器:
             片段=片段)
 
     @分析器母机.production('声明 : 块')
-    def 单块(片段):
+    def 单块(self, 片段):
         return 语法树.如果(
             条件=语法树.常量(True, 片段),
             主体=片段[0],
@@ -587,31 +587,31 @@ class 语法分析器:
             片段=片段)
 
     @分析器母机.production('每当声明 : 连词_每当 表达式 块')
-    def 每当(片段):
+    def 每当(self, 片段):
         return 语法树.每当(
             条件=片段[1],
             主体=片段[2],
             片段=片段)
 
     @分析器母机.production('每当声明 : 动词_循环 块')
-    def 每当(片段):
+    def 无条件每当(self, 片段):
         return 语法树.每当(
             条件=语法树.常量(True, 片段),
             主体=片段[1],
             片段=片段)
 
     @分析器母机.production('迭代器 : 表达式前缀')
-    def 迭代器(片段):
+    def 迭代器(self, 片段):
         return 片段[0]
 
     @分析器母机.production('遍历范围 : 表达式')
-    def 遍历范围(片段):
+    def 遍历范围(self, 片段):
         # TODO: 处理 ast.Starred
         return 片段[0]
 
     @分析器母机.production('对于声明 : 连词_对 迭代器 连词_于 遍历范围 块')
     @分析器母机.production('对于声明 : 连词_对 迭代器 : 遍历范围 块')
-    def 对于声明(片段):
+    def 对于声明(self, 片段):
         目标 = 片段[1]
         # TODO: 遍历 list
         目标.ctx = ast.Store()
@@ -622,10 +622,10 @@ class 语法分析器:
 
     @分析器母机.production('对于声明 : 声明 连词_对 迭代器 连词_于 遍历范围')
     @分析器母机.production('对于声明 : 声明 连词_对 迭代器 : 遍历范围')
-    def 单个对于声明(片段):
+    def 单个对于声明(self, 片段):
         倒装 = 片段[1:]
         倒装.append([片段[0]])
-        return 语法分析器.对于声明(倒装)
+        return self.对于声明(倒装)
 
     @分析器母机.production('名称 : 标识符')
     def 标识符(self, 片段):
