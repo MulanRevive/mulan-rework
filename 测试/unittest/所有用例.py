0fbe752677f3964badc5fb98c3f67b8e2effc818
nobodxbodon <nobodxbodon.github@gmail.com>
2020-11-25 13:35:02 -0800

    测试：win下，ulang原始文件通过，但重现测试两个字符串相关未过

--- 测试/unittest/所有用例.py
+++ 测试/unittest/所有用例.py
@@ -1,4 +1,4 @@
-from 测试.期望值表 import *
+from 测试.期望值表 import 期望值
 from 编辑器.运行木兰 import 运行木兰代码
 
 import unittest
@@ -7,6 +7,20 @@ class test所有(unittest.TestCase):
 
     def test(self):
         路径 = '测试/'
+        全部通过 = False
+        失败表 = {}
+
         for 文件 in 期望值:
             源码路径 = 路径 + 文件
-            self.assertEqual(运行木兰代码(源码路径), 期望值[文件].decode("utf-8"), 文件 + " 失败")
+            实际值 = 运行木兰代码(源码路径)
+            预期值 = 期望值[文件].decode("utf-8")
+
+            if 实际值 == 预期值:
+                print("通过： " + 文件)
+            else:
+                失败表[文件] = 实际值
+
+        print("===================")
+        for 文件 in 失败表:
+            print(f"失败： {文件} 期望：{repr(期望值[文件].decode('utf-8'))} 实际：{repr(失败表[文件])}")
+        self.assertTrue(全部通过, "！全部通过！")
