fe4b9d9efb76471ac248db10c1fdc7c37d28aacc
吴烜 <wuxuan.ecios@gmail.com>
2021-02-23 02:06:31 -0800

    报错：无属性信息部分改进

--- 木兰/功用/反馈信息.py
+++ 木兰/功用/反馈信息.py
@@ -11,6 +11,7 @@ from 木兰.分析器.语法分析器 import 语法分析器
 报错_除零 = "请勿除以零"
 报错_递归 = "递归过深。请确认: 1、的确需要递归 2、递归的收敛正确"
 报错_按索引取项 = "不支持按索引取项"
+报错_无属性 = "没有属性"
 参考_enter = "\n参考：https://stackoverflow.com/questions/1984325/explaining-pythons-enter-and-exit"
 
 def 反馈信息(例外, 源码文件=None):
@@ -58,12 +59,25 @@ def 提取(各层):
 
 def 取关键信息(基本信息, 首行):
     代码行 = 首行.内容
+    问题变量 = []
     if 基本信息.find(报错_按索引取项) > 0:
-        分析器 = 语法分析器(分词器)
-        节点 = 分析器.分析(代码行)
+        节点 = 语法分析器(分词器).分析(代码行)
         问题变量 = 诊断问题(节点)
-        return 基本信息 + ": " + (str(问题变量) if len(问题变量) != 1 else 问题变量[0])
-    return 基本信息
+    匹配 = re.search("(.*)" + 报错_无属性 + "‘(.*)’", 基本信息)
+    if 匹配:
+        节点 = 语法分析器(分词器).分析(代码行)
+
+        # TODO: 重构以避免重复匹配
+        问题属性 = 匹配.group(2)
+        问题变量 = 诊断无属性问题(节点, 问题属性)
+
+    if len(问题变量) == 0:
+        return 基本信息
+    elif len(问题变量) == 1:
+        return f"{基本信息}，看看‘{问题变量[0]}’"
+    else:
+        return f"{基本信息}，看看{问题变量}"
+
 
 def 诊断问题(节点):
     所有变量 = []
@@ -80,6 +94,30 @@ def 诊断问题(节点):
                 if 属性[0] == "value":
                     return [属性[1].id]
             所有变量 += 诊断问题(属性[1])
+    return 所有变量
+
+def 诊断无属性问题(节点, 问题属性):
+    所有变量 = []
+    if isinstance(节点, list):
+        for 子节点 in 节点:
+            所有变量 += 诊断无属性问题(子节点, 问题属性)
+    elif isinstance(节点, int) or isinstance(节点, str):
+        pass
+    else:
+        if 节点 == None:
+            return
+        有问题 = False
+        对象名 = ""
+        for 属性 in ast.iter_fields(节点):
+            if type(节点).__name__ == "Attribute":
+                # print(属性[0] + ": " + 属性[1])
+                if 属性[0] == "value":
+                    对象名 = 属性[1].id
+                elif 属性[0] == "attr" and 属性[1] == 问题属性:
+                    有问题 = True
+            所有变量 += 诊断无属性问题(属性[1], 问题属性)
+        if 有问题:
+            return [对象名]
     return 所有变量
 
 def 提示(类型, 原信息):
@@ -107,11 +145,15 @@ def 提示(类型, 原信息):
     elif 类型 == 'IndexError' and 原信息 == "list index out of range":
         return 报错_列表索引
     elif 类型 == 'AttributeError':
-        # TODO: 为 测试/错误处理/属性被静态调用.ul 改进错误信息
         信息 = "需要添加此属性：" + 原信息
         if 原信息 == "__enter__":
             信息 += 参考_enter
-        return 信息
+            return 信息
+
+        无属性 = "'(.*)' object has no attribute '(.*)'"
+        匹配 = re.search(无属性, 原信息)
+        if 匹配:
+            return f'{类型中文化(匹配.group(1))}{报错_无属性}‘{匹配.group(2)}’'
     elif 类型 == 'FileNotFoundError':
         return re.sub(r"\[Errno 2\] No such file or directory: '(.*)'",
             r"没找到文件或路径：‘\1’",
