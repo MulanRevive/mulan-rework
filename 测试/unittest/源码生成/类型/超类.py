85a17b9964542c4014f006d7f0d787bcc5c9c28e
吴烜 <wuxuan.ecios@gmail.com>
2021-05-04 01:11:44 -0700

    生成木兰：修正超类 python 用例

--- 测试/unittest/源码生成/类型/超类.py
+++ 测试/unittest/源码生成/类型/超类.py
@@ -1,7 +1,18 @@
+class Animal:
+  def __init__(self):
+    self.name = 'animal'
+
+class WildAnimal:
+  def __init__(self, name):
+    self.name = name
+
 class Person(Animal):
   def __init__(self):
-    super.__init__()
+    super().__init__()
 
 class Wolf(WildAnimal):
   def __init__(self):
     super().__init__("wowo")
+
+print(Person().name)
+print(Wolf().name)
