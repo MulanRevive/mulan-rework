a8e9c4f8653c9653334cdafb5c4aeb6cbc047bbd
nobodxbodon <nobodxbodon.github@gmail.com>
2021-05-20 01:20:01 -0700

    #I3RNBQ 修正生成测试win路径问题

--- 测试/unittest/生成源码表.py
+++ 测试/unittest/生成源码表.py
@@ -1,30 +1,28 @@
-import os
+from pathlib import Path
 
-源码目录 = "测试/unittest/源码生成/"
+源码目录 = Path("测试/unittest/源码生成/")
 
 原始木兰未过 = {
-    "测试/unittest/源码生成/函数/变长指名参数": "TypeError: can only concatenate str (not \"arg\") to str",
-    "测试/unittest/源码生成/类型/属性": "NameError: name 'decorator_list' is not defined",
+    Path("测试/unittest/源码生成/函数/变长指名参数.py"): "TypeError: can only concatenate str (not \"arg\") to str",
+    Path("测试/unittest/源码生成/类型/属性.py"): "NameError: name 'decorator_list' is not defined",
 }
 
 python源码表 = {}
 木兰源码表 = {}
 
-for 路径, 目录名, 所有文件 in os.walk(源码目录):
-    for 文件 in 所有文件:
-        文件名 = os.path.splitext(os.path.join(路径, 文件))
-        if 文件名[1] == '.py' and 文件名[0] not in 原始木兰未过:
-            python文件名 = 文件名[0]
-            python路径 = python文件名 + '.py'
-            with open(python路径, 'r', encoding='utf-8') as f:
-                python源码 = f.read()
+for 完整路径 in list(源码目录.glob('**/*.py')):
+    if 完整路径 not in 原始木兰未过:
+        python文件名 = str(完整路径)[:-len(完整路径.suffix)]
+        python路径 = python文件名 + '.py'
+        with open(python路径, 'r', encoding='utf-8') as f:
+            python源码 = f.read()
 
-            木兰路径 = python文件名 + ".ul"
-            if os.path.isfile(木兰路径):
-                with open(木兰路径, 'r', encoding='utf-8') as f:
-                    木兰源码 = f.read()
-            else:
-                木兰源码 = python源码
+        木兰路径 = python文件名 + ".ul"
+        if Path(木兰路径).exists():
+            with open(木兰路径, 'r', encoding='utf-8') as f:
+                木兰源码 = f.read()
+        else:
+            木兰源码 = python源码
 
-            python源码表[python路径] = python源码
-            木兰源码表[python路径] = 木兰源码
+        python源码表[python路径] = python源码
+        木兰源码表[python路径] = 木兰源码
