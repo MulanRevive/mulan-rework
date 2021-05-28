9a89ca015bca75ba380ea91ca0abb510d8cb97f9
fcfco <fcfco@qq.com>
2021-05-28 11:04:19 +0800

    修改生成声明为生成表达式

--- 木兰/分析器/语法树.py
+++ 木兰/分析器/语法树.py
@@ -62,7 +62,7 @@ class 语法树:
             )
             if 返回:
                 节点.returns = 返回
-        elif 类型 == 语法.生成声明:
+        elif 类型 == 语法.生成表达式:
             节点 = ast.Yield(value=值)
         elif 类型 == 语法.返回声明:
             节点 = ast.Return(value=值)
