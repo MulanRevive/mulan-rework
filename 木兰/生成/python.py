cd076d7428bd4573ea189bb6d8bae9fd7ea985ac
LaomoBK <laomobk@163.com>
2022-05-25 12:57:16 +0800

    将木兰到Python代码生成器内的方法按字母顺序排序

--- 木兰/生成/python.py
+++ 木兰/生成/python.py
@@ -31,6 +31,37 @@ class 代码生成器(codegen.SourceGenerator):
     def visit_arg(self, 参数节点):
         super().write(参数节点.arg)
 
+    def visit_AnnAssign(self, 节点):
+        self.newline(节点)
+        self.visit(节点.target)
+        self.write(' : ')
+        self.visit(节点.annotation)
+        self.write(' = ')
+        self.visit(节点.value)
+
+    def visit_Call(self, 节点):
+        if isinstance(节点.func, ast.Name):
+            if 节点.func.id in 木兰预置函数映射表:
+                节点.func.id = 木兰预置函数映射表[节点.func.id]
+        
+        需要逗号 = []
+        def 写逗号至结果():
+            if 需要逗号:
+                self.write(', ')
+            else:
+                需要逗号.append(True)
+
+        self.visit(节点.func)
+        self.write('(')
+        for 参数节点 in 节点.args:
+            写逗号至结果()
+            self.visit(参数节点)
+        for 关键字参数节点 in 节点.keywords:
+            写逗号至结果()
+            self.write(关键字参数节点.arg + '=')
+            self.visit(关键字参数节点.value)
+        self.write(')')
+
     def visit_ClassDef(self, 节点):
         有形式参数 = []
 
@@ -59,75 +90,11 @@ class 代码生成器(codegen.SourceGenerator):
         self.write(有形式参数 and '):' or ':')
         self.body(节点.body)
 
-    def visit_AnnAssign(self, 节点):
-        self.newline(节点)
-        self.visit(节点.target)
-        self.write(' : ')
-        self.visit(节点.annotation)
-        self.write(' = ')
-        self.visit(节点.value)
-
-    def visit_Name(self, 节点):
-        if 节点.id == 'PI':
-            self.write('pi')
-        else:
-            super().visit_Name(节点)
-
-    def visit_NameConstant(self, 节点):
-        if 节点.value == None:
-            self.write('None')
-        elif 节点.value:
-            self.write('True')
-        else:
-            self.write('False')
-
-    def visit_Call(self, 节点):
-        if isinstance(节点.func, ast.Name):
-            if 节点.func.id in 木兰预置函数映射表:
-                节点.func.id = 木兰预置函数映射表[节点.func.id]
-        
-        需要逗号 = []
-        def 写逗号至结果():
-            if 需要逗号:
-                self.write(', ')
-            else:
-                需要逗号.append(True)
-
-        self.visit(节点.func)
-        self.write('(')
-        for 参数节点 in 节点.args:
-            写逗号至结果()
-            self.visit(参数节点)
-        for 关键字参数节点 in 节点.keywords:
-            写逗号至结果()
-            self.write(关键字参数节点.arg + '=')
-            self.visit(关键字参数节点.value)
-        self.write(')')
-
-    def visit_With(self, 节点):
-        self.newline(节点)
-        self.write('with ')
-
-        for 索引, 项 in enumerate(节点.items):
-            if 索引 > 0:
-                self.write(', ')
-            self.visit(项)
-
-        self.write(':')
-        self.body(节点.body)
-
     def visit_ExtSlice(self, 节点):
         for 索引, 切片项 in enumerate(节点.dims):
             if 索引 != 0:
                 self.write(', ')
             self.visit(切片项)
-
-    def visit_withitem(self, 节点):
-        self.visit(节点.context_expr)
-        
-        if 节点.optional_vars is not None:
-            self.write(' as ')
-            self.visit(节点.optional_vars)
 
     def visit_ImportFrom(self, 节点):
         self.newline(节点)
@@ -140,6 +107,20 @@ class 代码生成器(codegen.SourceGenerator):
             if 索引:
                 self.write(', ')
             self.visit(项)
+
+    def visit_NameConstant(self, 节点):
+        if 节点.value == None:
+            self.write('None')
+        elif 节点.value:
+            self.write('True')
+        else:
+            self.write('False')
+
+    def visit_Name(self, 节点):
+        if 节点.id == 'PI':
+            self.write('pi')
+        else:
+            super().visit_Name(节点)
 
     def signature(self, 节点):
         需要逗号 = []
@@ -166,6 +147,25 @@ class 代码生成器(codegen.SourceGenerator):
         if 节点.kwarg is not None:
             写逗号至结果()
             self.write('**' + 节点.kwarg.arg)
+
+    def visit_withitem(self, 节点):
+        self.visit(节点.context_expr)
+        
+        if 节点.optional_vars is not None:
+            self.write(' as ')
+            self.visit(节点.optional_vars)
+
+    def visit_With(self, 节点):
+        self.newline(节点)
+        self.write('with ')
+
+        for 索引, 项 in enumerate(节点.items):
+            if 索引 > 0:
+                self.write(', ')
+            self.visit(项)
+
+        self.write(':')
+        self.body(节点.body)
 
     def 得到源码(self, 节点):
         self.visit(节点)
