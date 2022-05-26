cbada7d9d8a39829a104bcae7cb753247d26a50a
LaomoBK <laomobk@163.com>
2022-05-27 00:14:09 +0800

    优化测试器对原始木兰输出的读取

--- 测试/unittest/木兰到Python.py
+++ 测试/unittest/木兰到Python.py
@@ -73,7 +73,7 @@ def 生成原始木兰的测试结果(原始木兰执行命令: str, 源码字�
                 错误输出流.decode('gbk').startswith('UnicodeDecodeError'):
             continue
 
-        结果[路径] = 原始木兰输出结果.replace('\r\n', '\n')
+        结果[路径] = 原始木兰输出结果.replace('\r\n', '\n')[:-2]  # 除去末尾因为popen读取而导致的 \n
 
     print('原始木兰输出结果生成完毕!')
     
