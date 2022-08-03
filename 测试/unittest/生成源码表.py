749ac931a7c0272fbb8b7fa4c03c337a2f3f31c7
laomobk <laomobk@163.com>
2022-08-03 17:54:25 +0800

    测试：跳过 `测试/unittest/源码生成/算法/排序/冒泡.py` 和 `测试/unittest/源码生成/综合/猜数字.py` 的测试，原因：原始木兰对读取中文标识符支持出现问题

--- 测试/unittest/生成源码表.py
+++ 测试/unittest/生成源码表.py
@@ -6,6 +6,8 @@ from pathlib import Path
     Path("测试/unittest/源码生成/函数/变长指名参数.py"): "TypeError: can only concatenate str (not \"arg\") to str",
     Path("测试/unittest/源码生成/类型/属性.py"): "NameError: name 'decorator_list' is not defined",
     Path("测试/unittest/源码生成/流程控制/try.py"): "AttributeError: 'str' object has no attribute '_fields'",
+    Path("测试/unittest/源码生成/算法/排序/冒泡.py"): "UnicodeDecodeError: 'gbk' codec can't decode byte 0x80 in position 6: illegal multibyte sequence",
+    Path("测试/unittest/源码生成/综合/猜数字.py"):  "UnicodeDecodeError: 'gbk' codec can't decode byte 0xb0 in position 30: illegal multibyte sequence",
 }
 
 python源码表 = {}
