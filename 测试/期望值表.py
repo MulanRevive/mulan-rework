bb36a3e637e3beae4ddec5c4c28e75206296deb8
fcfco <fcfco@qq.com>
2022-11-15 23:08:19 +0800

    实现支持带 with 和 final： try_stmt : TRY withitem block final_stmt 文档中把试试声明合并到一起 添加测试用例

--- 测试/期望值表.py
+++ 测试/期望值表.py
@@ -47,6 +47,7 @@ import datetime
     "流程控制/catch.ul": b"vi",
     "流程控制/catch名称.ul": b"list index out of range",
     "流程控制/catch块.ul": b'v',
+    "流程控制/试试顺便善后.ul": b'0f',
 
     "函数/无参数.ul": b"223", # 最后无4是因为返回了函数, 而非它的返回值
     "函数/单参数.ul": b"2",
